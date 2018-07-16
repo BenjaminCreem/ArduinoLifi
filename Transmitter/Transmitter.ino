bb980f7173059618812e4954811e88709b1ddf71
brettgrossman <grossmanb1@wit.edu>
2018-07-16 12:07:48 -0400

    File I/O

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -74,3 +74,24 @@ void sendMain(int val)
       }
   }
 }
+void sendFile (char input)
+{
+   char holder = 0;
+   boolean loop1 = true; 
+   File file = FileSystem.Open(input, FILE_READ);
+   if(file==false)
+      error();
+   sendValue (int(file.size()));
+   while(loop1==true)
+   {
+      holder= file.Read();
+      if(holder!=-1)
+          sendValue(encode(holder));
+      else
+          loop1=false;
+   }
+    return; 
+}
+
+
+
